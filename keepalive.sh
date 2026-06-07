c93f83d14b9c0211ff45e2e9d3bc944bccb079d1
Z User <z@container>
2026-06-07 16:47:41 +0000

    b28a386d-8328-443a-814a-095768e7a01a

--- keepalive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  echo "$(date): Starting DapurMind AI server..."
-  npx next dev -p 3000 2>&1
-  echo "$(date): Server crashed, restarting in 3s..."
-  sleep 3
-done
